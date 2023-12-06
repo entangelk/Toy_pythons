81462272b751f1a94f12d583644a8f433a9d30a0
ohjisu320 <ohjisu320@gmail.com>
2023-12-06 17:38:08 +0900

    commit

--- docs/polls_first_jisu.py
+++ docs/polls_first_jisu.py
@@ -9,13 +9,17 @@ list_answer =  ["좋음", "중간", "좋아지길"]
 
 
 for count in [0, 1, 2, 3]:
-    # 질문 index
-        print("{}. {}".format(count+1, list_question[count]))     
+    # 질문 명령
+        print("{}. {}".format(count+1, list_question[count]))
+    # 답안 명령    
         for count_a in [0, 1, 2]:
                    print("{}. {}".format(count_a+1, list_answer[count_a]), end=" ")
+    # count가 3보다 작을 때 "----" 출력
         if count < 3 :
+    # 다른 행에 나올 수 있도록 분리
                 print("")
                 print("----")
+# 다른 행에 나올 수 있도록 분리
 print("")
 print("End program!")
     
