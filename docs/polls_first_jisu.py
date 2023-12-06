3d8d71339f85c985473f298124f482b2bd857dce
ohjisu320 <ohjisu320@gmail.com>
2023-12-06 17:36:30 +0900

    commit

--- docs/polls_first_jisu.py
+++ docs/polls_first_jisu.py
@@ -6,45 +6,11 @@ list_question = [
 
 list_answer =  ["좋음", "중간", "좋아지길"]
 
-# #질문 만듦
-# for count_q in [0, 1, 2, 3]:
-#     pass
-#     # 질문 index
-#     str_questions = list_question[count_q]
-#     print("{}. {}".format(count_q+1, str_questions))
-# print("End program!")
-# #답안 만듦
-# for count_a in [0, 1, 2] :
-#     pass
-#     # 답안 index
-#     str_answer = list_answer[count_a]
-#     print("{}. {}".format(count_a+1, str_answer), end=" ")
-
-
-# for count in [0, 1, 2, 3]:
-#     if count <4:
-#     # 질문 index
-#         str_questions = list_question[count]
-#         print("{}. {}".format(count+1, str_questions))
-#         print("1. {} 2. {} 3. {}".format(list_answer[0], list_answer[1], list_answer[2]))
-#     print("---------")
-# else :
-#     print("")
-    
-# print("End program!")
-
-# #답안 만듦
-# for count_a in [0, 1, 2] :
-#     pass
-#     # 답안 index
-#     str_answer = list_answer[count_a]
-#     print("{}. {}".format(count_a+1, str_answer), end=" ")
 
 
 for count in [0, 1, 2, 3]:
     # 질문 index
-        str_questions = list_question[count]
-        print("{}. {}".format(count+1, str_questions))     
+        print("{}. {}".format(count+1, list_question[count]))     
         for count_a in [0, 1, 2]:
                    print("{}. {}".format(count_a+1, list_answer[count_a]), end=" ")
         if count < 3 :
