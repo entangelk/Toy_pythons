15d8146bb02b05a6cafd4ae1b2c941fc0a0be952
entangelk <kdtyohan@gmail.com>
2023-12-06 16:30:45 +0900

    update 2

--- docs/polls_first_[yohan].py
+++ docs/polls_first_[yohan].py
@@ -9,10 +9,17 @@ list_answer =  ["좋음", "중간", "좋아지길"]
 
 
 for num_count in range(len(list_question)): # list_question의 아이템 갯수만큼 반복
-    print("{}".format(list_question[num_count]))
+    print("{}".format(list_question[num_count]),)
     for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복
-        print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정
+        if num_second_count != 2:
+            print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+            pass
+        else :
+            print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+            pass
         pass
     pass
+
+    
 
 #뭘 넘겨줘야 할지 몰라서 제일 어려운 중간에 선 3개 긋는거...?
