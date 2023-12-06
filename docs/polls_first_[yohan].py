434dd999c74a53d1b742fc8307b5612654bcec0d
entangelk <kdtyohan@gmail.com>
2023-12-06 16:38:25 +0900

    complet

--- docs/polls_first_[yohan].py
+++ docs/polls_first_[yohan].py
@@ -10,16 +10,25 @@ list_answer =  ["좋음", "중간", "좋아지길"]
 
 for num_count in range(len(list_question)): # list_question의 아이템 갯수만큼 반복
     print("{}".format(list_question[num_count]),)
-    for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복
-        if num_second_count != 2:
-            print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+    if num_count != 3:
+        for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복
+            if num_second_count != 2:
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+                pass
+            else :
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+                print("--------------------------------------")
+                pass
             pass
-        else :
-            print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+        pass
+    else : 
+        for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복
+            if num_second_count != 2:
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+                pass
+            else :
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+                pass
             pass
         pass
     pass
-
-    
-
-#뭘 넘겨줘야 할지 몰라서 제일 어려운 중간에 선 3개 긋는거...?
