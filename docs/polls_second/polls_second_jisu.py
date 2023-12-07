cf492f3ec42db38e35c694ee6c4a29de2c2c4f42
entangelk <kdtyohan@gmail.com>
2023-12-07 09:42:05 +0900

    second update

---
+++ docs/polls_second/polls_second_jisu.py
@@ -0,0 +1,38 @@
+# sorce yohan from ../pools_first/polls_first_[yohan].py
+# 코드가 지저분해서... 미안해요... 미안.... 안녕...
+
+list_question = [
+    "상품의 품질에 대해 어떻게 생각하시나요?",
+    "상품의 가격에 대해 어떻게 생각하시나요?",
+    "상품의 디자인에 대해 어떻게 생각하시나요?",
+    "상품에 대한 전반적인 만족도는 어떠신가요?"
+]
+
+
+list_answer =  ["좋음", "중간", "좋아지길"]
+
+
+for num_count in range(len(list_question)): # list_question의 아이템 갯수만큼 반복
+    print("{}. {}".format(num_count+1,list_question[num_count]),)
+    if num_count != 3:
+        for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복
+            if num_second_count != 2:
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+                pass
+            else :
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+                print("--------------------------------------")
+                pass
+            pass
+        pass
+    else : 
+        for num_second_count in range(len(list_answer)): # list_answer의 아이템 갯수만큼 반복, 마지막줄 절취선 삭제 루프
+            if num_second_count != 2:
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count]),end=" ") # 아이템 갯수 index+1을 번호로 지정 2번째 라인까지
+                pass
+            else :
+                print("{}. {}".format(num_second_count+1,list_answer[num_second_count])) # 아이템 갯수 3번라인 줄바꿈
+                pass
+            pass
+        pass
+    pass
---
+++ docs/polls_second/polls_second_yohan.py
@@ -0,0 +1,27 @@
+# sorce jisu from ../pools_first/polls_first_jisu.py
+
+list_question = [
+    "상품의 품질에 대해 어떻게 생각하시나요?",
+    "상품의 가격에 대해 어떻게 생각하시나요?",
+    "상품의 디자인에 대해 어떻게 생각하시나요?",
+    "상품에 대한 전반적인 만족도는 어떠신가요?"]
+
+list_answer =  ["좋음", "중간", "좋아지길"]
+
+
+
+for count in [0, 1, 2, 3]:
+    # 질문 명령
+        print("{}. {}".format(count+1, list_question[count]))
+    # 답안 명령    
+        for count_a in [0, 1, 2]:
+                   print("{}. {}".format(count_a+1, list_answer[count_a]), end=" ")
+    # count가 3보다 작을 때 "----" 출력
+        if count < 3 :
+    # 다른 행에 나올 수 있도록 분리
+                print("")
+                print("----")
+# 다른 행에 나올 수 있도록 분리
+print("")
+print("End program!")
+    
