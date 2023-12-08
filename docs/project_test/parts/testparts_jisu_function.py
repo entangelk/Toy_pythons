5ef6c25196ab017c1d20ed976bb9be19fe061a72
ohjisu320 <ohjisu320@gmail.com>
2023-12-08 12:42:38 +0900

    push

--- docs/project_test/parts/testparts_jisu_function.py
+++ docs/project_test/parts/testparts_jisu_function.py
@@ -19,17 +19,13 @@ def function() :
     for num_count in range(len(list_question)) :
         print("{}. {}".format(num_count+1, list_question[num_count]),end=" ") 
         print("")
-        print("{}".format(list_answer[num_count]),end=" ") 
-        pass
+        print("{}".format(list_answer[num_count]),end=" ")
         print("")
-        print("")
-        num_print_answer.append(int(input("정답 : "))) 
-
-
-        pass  
-        pass
-    pass
+        num_print_answer.append(int(input("-정답 : ")))
+        print("")      
     return num_print_answer
+
+function()
 
 
 
